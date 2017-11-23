606d12a992dbd38864528a7cfffbe5f8d779fb11
rafi <rafi@rafialam.com>
2017-11-23 03:50:38 -0500

    Movement

---
+++ Assets/Code/Glitcher.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Glitcher : MonoBehaviour, IPointerDownHandler {
+
+	[SerializeField] float speed = 3; 
+	[SerializeField] Transform glitch;
+
+	Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+		targetPosition = glitch.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		KeyMove();
+	}
+
+	public virtual void OnPointerDown(PointerEventData eventData) {
+			if (glitch.position != Input.mousePosition) {
+				targetPosition = Input.mousePosition;
+			}
+	}
+
+	void KeyMove() {
+
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+		move = move.normalized * Time.deltaTime * speed * 10;
+	
+		if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0) {
+			glitch.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		}
+
+		if (Input.GetAxis("Vertical") != 0 && Input.GetAxis("Horizontal") == 0) {
+			glitch.GetComponent<Rigidbody>().velocity = move;
+			if (Input.GetAxis("Vertical") > 0) {
+				//animstate = 1;
+			} else {
+				//animstate = 0;
+			}
+		}
+
+		if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") != 0) {
+			glitch.GetComponent<Rigidbody>().velocity = move;
+			//animstate = 2;
+			if (Input.GetAxis("Horizontal") > 0) {
+				//sprite.transform.localScale = new Vector3 (-1,1,1);
+			} else {
+				//sprite.transform.localScale = new Vector3 (1,1,1);
+			}
+		}
+
+	}
+}
